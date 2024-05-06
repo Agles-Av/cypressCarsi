2310020704bae7c962fa61b7b0242535180c3129
Agles <20223tn005@utez.edu.mx>
2024-05-06 11:53:19 -0600

    primeras pruebas

---
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -0,0 +1,27 @@
+require('cypress-xpath');
+/**
+ * Sitio a probar https://www.demoblaze.com/index.html
+ * - Visitar el sitio
+ */
+
+//Crear la funcion de la prueba
+describe('Prueba al sitio de DemoBlaze',()=>{
+    //TODO el código para realizar mi prueba
+    //paso 1 - Visitar el sitio de DemoBlaze
+    beforeEach(()=>{
+        cy.visit('https://example.cypress.io/');
+    });
+    it('entrar al apartado de Actions',()=>{
+        cy
+        .xpath('/html/body/nav')
+        .contains('Commands')
+        .click()
+        .xpath('//*[@id="navbar"]/ul[1]/li[1]/ul/li[3]/a')
+        .click()
+        .wait(2000);
+    });
+
+    it('Saber que entro correctamente',()=>{
+        cy.xpath('//*[@id="type"]/a')
+    })
+});
